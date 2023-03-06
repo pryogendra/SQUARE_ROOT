18e4991ec1a16a0de7780cb49d041fa447a73635
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2023-03-06 09:28:16 +0530

    Add files via upload

---
+++ square_root.php
@@ -0,0 +1,8 @@
+<html>
+    <?php
+    echo(sqrt(4))."<br>";  //sqrt() returns the square root of the entered number
+    echo(sqrt(121))."<br>";
+    echo(sqrt(34))."<br>";
+    echo(sqrt(40))."<br>";
+    ?>
+</html>
